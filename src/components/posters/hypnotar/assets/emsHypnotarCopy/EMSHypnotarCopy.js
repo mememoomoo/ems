cb0cc6b523bebc53e4b1ae19700f85311c8dea17
Jack Larsen <jack@larsenwebdesign.com>
2022-02-18 18:17:17 -0700

    Home Poster
    Add bottom part of poster. Need to add navigation to those buttons.

--- src/components/posters/hypnotar/assets/emsHypnotarCopy/EMSHypnotarCopy.js
+++ src/components/posters/hypnotar/assets/emsHypnotarCopy/EMSHypnotarCopy.js
@@ -1,7 +1,25 @@
+import "./ems-hypnotar-copy.css";
+import Copyright from "../../../../copyright/Copyright";
+
 function EMSHypnotarCopy() {
   return (
     <div className="hypnotar-copy-container">
-      <p>This content. haha</p>
+      <div className="hypnotar-content-box">
+        <div className="hypnotar-contact-box">
+          <p>Contact</p>
+        </div>
+        <div className="hypnotar-center-box">
+          <div className="hypnotar-tagline-box">
+            <p>Emergency Medical Services For Any Event</p>
+          </div>
+          <div className="hypnotar-copyright-box">
+            <Copyright />
+          </div>
+        </div>
+        <div className="hypnotar-about-box">
+          <p>About</p>
+        </div>
+      </div>
     </div>
   );
 }
